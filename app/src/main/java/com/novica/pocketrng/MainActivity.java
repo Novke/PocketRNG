969cd4a3355bdaddcf0705537b3281561bee2037
Novke <novke.brate@gmail.com>
2024-03-13 11:55:01 +0100

    Input Functionality

--- app/src/main/java/com/novica/pocketrng/MainActivity.java
+++ app/src/main/java/com/novica/pocketrng/MainActivity.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.novica.pocketrng.services.JavaRNGService;
 import com.novica.pocketrng.services.RNGService;
@@ -18,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
     private Button button;
     private TextView numJava;
     private TextView numRandomOrg;
+    private EditText txtMin;
+    private EditText txtMax;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,13 +31,39 @@ public class MainActivity extends AppCompatActivity {
         button = findViewById(R.id.button);
         numJava = findViewById(R.id.numJava);
         numRandomOrg = findViewById(R.id.numRandomOrg);
+        txtMin = findViewById(R.id.txtMin);
+        txtMax = findViewById(R.id.txtMax);
 
         RNGService javaRng = new JavaRNGService();
         RNGService randomOrgRng = new RandomOrgApiService();
 
+
+        txtMin.setText("1");
+        txtMax.setText("100");
+
+
+
+
         button.setOnClickListener(listener -> {
+
+            int minimum;
+            int maximum;
+
             try {
-                int javaNumber = javaRng.generateRandomNumber(1, 100);
+                 minimum = Integer.parseInt(txtMin.getText().toString());
+                 maximum = Integer.parseInt(txtMax.getText().toString());
+            } catch (Exception ex){
+                Toast.makeText(this, "Invalid input", Toast.LENGTH_LONG).show();
+                return;
+            }
+
+            if (minimum>=maximum){
+                Toast.makeText(this, "Minimum must be less than maximum", Toast.LENGTH_LONG).show();
+                return;
+            }
+
+            try {
+                int javaNumber = javaRng.generateRandomNumber(minimum, maximum);
                 numJava.setText(String.valueOf(javaNumber));
             } catch (IOException e) {
                 e.printStackTrace();
@@ -45,7 +75,7 @@ public class MainActivity extends AppCompatActivity {
                 protected Integer doInBackground(Void... voids) {
                     try {
                         numRandomOrg.setText("...");
-                        return randomOrgRng.generateRandomNumber(1, 100);
+                        return randomOrgRng.generateRandomNumber(minimum, maximum);
                     } catch (IOException e) {
                         e.printStackTrace();
                         return null;
@@ -58,6 +88,7 @@ public class MainActivity extends AppCompatActivity {
                         numRandomOrg.setText(String.valueOf(result));
                     } else {
                         numRandomOrg.setText("/");
+                        Toast.makeText(MainActivity.this, "Random.org requires internet connection", Toast.LENGTH_SHORT).show();
                     }
                 }
             }.execute();
